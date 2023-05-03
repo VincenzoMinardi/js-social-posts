96b9d669508d5125e8ac705eabf58746c2acd7df
VincenzoMinardi <vincenzo.minardi.mv@gmail.com>
2023-05-03 15:45:49 +0200

    add: function

--- assets/main.js
+++ assets/main.js
@@ -39,7 +39,7 @@ const posts = [
         "media": "https://unsplash.it/600/400?image=24",
         "author": {
             "name": "Luca Formicola",
-            "image": null
+            "image": "https://unsplash.it/300/300?image=20" 
         },
         "likes": 56,
         "created": "2021-04-03"
@@ -82,7 +82,7 @@ for (let i = 0; i < posts.length; i++) {
 <div class="post__footer">
     <div class="likes js-likes">
         <div class="likes__cta">
-            <a class="like-button  js-like-button" href="#" data-postid="1">
+            <a class="like-button  js-like-button" href="#" data-postid="${posts[i].id}">
                 <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
                 <span class="like-button__label">Mi Piace</span>
             </a>
@@ -95,5 +95,13 @@ for (let i = 0; i < posts.length; i++) {
 </div>`
 
 }
+
+const eleLikeButton = document.querySelector(".like-button");
+
+eleLikeButton.addEventListener("click", function(){
+
+    console.log("LIKE")
+});
+
 
 
